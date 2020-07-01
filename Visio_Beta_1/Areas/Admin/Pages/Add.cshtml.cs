da058ad4ebd36d7ae4150a42e473f2eb702abb82
Eel2000 <eliezerbwana@gmaill.com>
2020-07-01 11:12:05 +0200

    move add page from shared dir to pages dir

---
+++ Visio_Beta_1/Areas/Admin/Pages/Add.cshtml.cs
@@ -0,0 +1,39 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Visio_Beta_1.Data;
+using Visio_Beta_1.Data.Models;
+
+namespace Visio_Beta_1.Areas.Admin.Pages.Shared
+{
+    public class AddModel : PageModel
+    {
+        private readonly ApplicationDbContext _db;
+
+        public AddModel(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        [BindProperty]
+        public BookViewModel BookViewModel { get; set; }
+
+        [BindProperty]
+        public Book Book { get; set; }
+
+        public void OnGet()
+        {
+        }
+    }
+
+    public class BookViewModel
+    {
+        [Required]
+        public IFormFile Fichier { get; set; }
+    }
+}
